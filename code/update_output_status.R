da014a5445cfdd38c10c12ec1ce6e2d25a9b2ceb
Anders Kiledal <akiledal@udel.edu>
2025-01-31 15:34:48 -0500

    Include R script for checking output status

---
+++ code/update_output_status.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+setwd("~/GLAMR")
+library(tidyverse)
+library(DBI)
+
+# googlesheets4::gs4_deauth()
+# googlesheets4::gs4_auth(email = "kiledal@umich.edu",cache = "~/GLAMR/.secrets")
+
+options(gargle_oath_cache = "~/GLAMR/.secrets",
+        gargle_oauth_email = "kiledal@umich.edu",
+        use_oob = TRUE,
+        gargle_oauth_client_type = "web")
+
+
+output_key <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1rkLra_xXPHjYinXzY1UpC6Mg5nzybcvG3HcGQaW8Tvc/edit#gid=0") %>% 
+  separate_longer_delim(sample_types,delim = ",")
+
+pg <- DBI::dbConnect(RPostgres::Postgres(),dbname = "glamr_data", host = "localhost", port = "5432", user = "glamr_admin", password = "glamr2023")
+samples <- tbl(pg, "glamr_samples")
+
+samp_to_check <- samples %>% 
+  dplyr::select(SampleID, StudyID,sample_type) %>% 
+  collect()
+
+outputs_to_check <- samp_to_check %>% 
+  cross_join(output_key ) %>% 
+  filter(sample_type == sample_types)
+
+output_status <- outputs_to_check %>% 
+  mutate(project = StudyID) %>% 
+  rowwise() %>% 
+  mutate(full_path = str_glue(example_path)) %>% 
+  ungroup() %>% 
+  mutate(output_exists = fs::file_exists(full_path),
+         checked = now())
+
+dbWriteTable(pg,"output_status",output_status,overwrite = TRUE)
+
+outputs_wide <- output_status %>%
+  distinct() %>% 
+  dplyr::select(-full_path, -example_path, -module, -standard_product, -checked) %>% 
+  pivot_wider(id_cols = SampleID:sample_type,names_from = product, values_from = output_exists)
+
+dbWriteTable(pg,"output_status_wide",outputs_wide,overwrite = TRUE)
+
