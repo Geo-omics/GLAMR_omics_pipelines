9cb6b0c4932fced80d3254420b0e536ce2babab9
Robert <heinro@umich.edu>
2025-12-22 15:00:51 -0500

    pypelib/hmm_summarize: add plotting WIP WIP WIP

--- pypelib/amplicon/hmm_summarize.py
+++ pypelib/amplicon/hmm_summarize.py
@@ -5,9 +5,16 @@ import argparse
 from collections import Counter
 from itertools import groupby
 import json
+from pathlib import Path
 from statistics import median, quantiles
 
+from matplotlib.backends.backend_pdf import PdfPages
+import pandas
+
+from ..utils import UsageError
+from . import dispatch
 from .alignment import HMMRAlignment
+from .hmm import HMM
 
 
 def cli():
@@ -17,17 +24,45 @@ def cli():
     The summary is written to stdout.
     """
     argp = argparse.ArgumentParser(description=__doc__)
-    argp.add_argument(
+    subp = argp.add_subparsers(dest='subcmd')
+    sump = subp.add_parser(
+        'summary',
+        help='Summarize alignments for one sample',
+    )
+    sump.add_argument(
         'inputfile',
         help='Tabular alignment file as made with nhmmscan\'s --tblout option',
     )
-    argp.add_argument(
+    sump.add_argument(
         '--output',
         help='Name of output file. By default print to stdout.',
     )
-    argp.add_argument('--debug', action='store_true', help='debug mode')
+    sump.add_argument('--debug', action='store_true', help='debug mode')
+    plotp = subp.add_parser(
+        'plot',
+        help='Plot sequencing depth for a sample',
+    )
+    plotp.add_argument(
+        'inputfile',
+        help='Tabular alignment file as made with nhmmscan\'s --tblout option',
+    )
+    multip = subp.add_parser(
+        'multiplot',
+        help='Plot sequencing depth for whole dataset',
+    )
+    multip.add_argument(
+        'inputfile',
+        help='The target assignment file for the dataset.',
+    )
     args = argp.parse_args()
-    main(args.inputfile, args.output, args.debug)
+    try:
+        match args.subcmd:
+            case 'summary': main(args.inputfile, args.output, args.debug)
+            case 'plot': plot(args.inputfile)
+            case 'multiplot': multiplot(args.inputfile)
+            case _: raise ValueError('invalid subcommand')
+    except UsageError as e:
+        argp.error(e)
 
 
 def main(infile, outfile=None, debug=False):
@@ -233,6 +268,118 @@ def get_mode(alignments):
     items['hmmfrom_avg'] = median(hmmfroms)
     items['hmmto_avg'] = median(hmmtos)
     return items
+
+
+def plot(inputfile):
+    """ Plot from single HMMR tbl output file """
+    alignments = HMMRAlignment.load(inputfile)
+    depth = {}
+    for a in alignments:
+        if a.model not in depth:
+            depth[a.model] = {'+': Counter(), '-': Counter()}
+        for pos in range(a.hmmfrom, a.hmmto + 1):
+            depth[a.model][a.strand][pos] += 1
+
+    for hmm, data in depth.items():
+        df_data = {}
+        index = pandas.RangeIndex(1, hmm.length + 1)
+        for strand, counts in data.items():
+            df_data[strand] = pandas.Series(counts, index)
+        df = pandas.DataFrame(df_data)
+        ax = df.plot()
+        ax.set_title(hmm.name)
+        ax.set_yscale('log')
+        for i in hmm.fwd_primers:
+            ax.fill_between(
+                [i.start, i.end], 0, 1200,
+                color='green', alpha=0.25,
+            )
+        for i in hmm.rev_primers:
+            ax.fill_between(
+                [i.start, i.end], 0, 1200,
+                color='brown', alpha=0.25,
+            )
+        ax.figure.savefig(f'{hmm.name}.depth.pdf')
+        ax.cla()
+
+
+def multiplot(inputfile):
+    """ Plot from multiple HMMR tbl output files """
+    infile = Path(inputfile)
+    samp_dir = infile.parent / 'amplicons'
+    groupings = None
+    if infile.is_file():
+        assignments = dispatch.get_assignments(path=infile)
+        groupings = {}
+        for sample_id, target in assignments.items():
+            if target in (dispatch.SKIP, dispatch.UNKNOWN):
+                groupings[sample_id] = target
+            else:
+                hmm, _, _ = HMM.parse_target(target)
+                groupings[sample_id] = hmm
+    elif samp_dir.is_dir():
+        print('[NOTICE] no such assignmetn file entering dataset-auto-mode')
+    else:
+        raise UsageError(f'no such file {inputfile} and no such directory '
+                         f'{samp_dir}')
+
+    data = {}
+    for i in samp_dir.glob('samp_*/detect_region/tbl_single.txt'):
+        sample_id = i.parts[-3]
+        if groupings is None:
+            group = 'all'
+        else:
+            group = groupings[sample_id]
+
+        if group not in data:
+            data[group] = []
+
+        grpdat = {}
+        for a in HMMRAlignment.load(i):
+            if isinstance(group, str):
+                # keep data for any and all models
+                pass
+            else:
+                # group is a HMM
+                if a.model != group:
+                    # skip other model's alignments
+                    continue
+            if a.model not in grpdat:
+                grpdat[a.model] = []
+            depth = {'+': Counter(), '-': Counter()}
+            for pos in range(a.hmmfrom, a.hmmto + 1):
+                depth[a.strand][pos] += 1
+            grpdat[a.model].append((sample_id, depth))
+        data[group] = grpdat
+
+    pp = PdfPages('.pdf')
+    for group, grpdat in data.items():
+        for hmm, depths in grpdat.items():
+            index = pandas.RangeIndex(1, hmm.length + 1)
+
+            df_data = {}
+            for sample_id, depth in depths:
+                for strand, counts in depth.items():
+                    df_data[f'{sample_id}{strand}'] = \
+                        pandas.Series(counts, index)
+
+            df = pandas.DataFrame(df_data)
+            ax = df.plot()
+            ax.set_title(hmm.name)
+            ax.set_yscale('log')
+            for i in hmm.fwd_primers:
+                ax.fill_between(
+                    [i.start, i.end], 0, 1200,
+                    color='green', alpha=0.25,
+                )
+            for i in hmm.rev_primers:
+                ax.fill_between(
+                    [i.start, i.end], 0, 1200,
+                    color='brown', alpha=0.25,
+                )
+            ax.figure.savefig(f'{hmm.name}.depth.pdf')
+            ax.cla()
+
 
 
 if __name__ == '__main__':
