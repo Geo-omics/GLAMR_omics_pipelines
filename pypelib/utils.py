1c5de8b46f5a2f87d07a863a922bd3444cfa99a9
Robert <heinro@umich.edu>
2026-01-28 14:48:04 -0500

    pypelib/utils: add logging helper for snakemake rules
    The next commit will have usage examples.

--- pypelib/utils.py
+++ pypelib/utils.py
@@ -1,5 +1,11 @@
-from contextlib import contextmanager, ExitStack
+from contextlib import (contextmanager, ExitStack, redirect_stdout,
+                        redirect_stderr)
+from functools import wraps
+from inspect import signature
 import json
+from pathlib import Path
+import sys
+import traceback
 
 
 @contextmanager
@@ -82,3 +88,84 @@ def load_stats(file_or_path):
 
 class UsageError(Exception):
     pass
+
+
+class logme:
+    """
+    3-in-1 context manager to to write stdout/stderr/tracebacks to a log file.
+
+    For use in with statements in Snakefile rules.  Can also be used as a
+    function decorator, which is the preferred usage for rules that make a
+    single function call in their run section.
+
+    After the code block or wrapped function is done, the log file will be read
+    and stdout and stderr will be also be printed to the regular stdout.  If an
+    exception occurred inside the block the tracback will not be printed to
+    stdout as snakemake is expected to print it always.
+
+    The rule's log section must define a single log file, which is then either
+    passed to logme() in the with statement or is passed via log=log kwarg to
+    the decorated function.  The passed log can also be a str or PathLike.  The
+    wrapper will consume the log kwarg unless the decocrated function has also
+    a log parameter.  This allows the decorated function to write additional
+    data to the log file without also printing it to stdout.  The decorated
+    function must xpect log to be file-like, open for writing.
+    """
+    def __init__(self, log=None):
+        # log is the rule's log but can also be a str to PathLike
+        self.log = log
+
+    def __enter__(self):
+        if self.log is None:
+            raise ValueError('need path to log file')
+
+        self.logfile = open(str(self.log), 'w+')
+        self.redir_out_cm = redirect_stdout(self.logfile)
+        self.redir_out_cm.__enter__()
+        self.redir_err_cm = redirect_stderr(self.logfile)
+        self.redir_err_cm.__enter__()
+        return self.logfile
+
+    def __exit__(self, exc_type, exc, exc_tb):
+        self.redir_err_cm.__exit__(exc_type, exc, exc_tb)
+        self.redir_out_cm.__exit__(exc_type, exc, exc_tb)
+        self.logfile.seek(0)
+        print(self.logfile.read())  # print all output
+        if exc is not None:
+            # print traceback to log file
+            traceback.print_tb(exc_tb, file=self.logfile)
+        self.logfile.close()
+
+    def _recreate_cm(self):
+        """ cf. contextlib.ContextDecorator """
+        return self
+
+    def __call__(self, func):
+        """ cf. contextlib.ContextDecorator """
+        @wraps(func)
+        def wrapper(*args, log=None, **kwargs):
+            keep_logkw = signature(func).parameters.get('log') is not None
+            # NOTE: too lazy to check the parameter kind, func() call will fail
+            # if log is positional-only
+
+            if log is None:
+                # Allow funct to be called without the log kwargs, in which
+                # case the function runs as-is
+                if keep_logkw:
+                    kwargs['log'] = None
+                return func(*args, **kwargs)
+            else:
+                self.log = log
+                with self._recreate_cm() as logfile:
+                    if keep_logkw:
+                        kwargs['log'] = logfile
+                    return func(*args, **kwargs)
+        return wrapper
+
+
+@logme()
+def test_logme(good='good', bad='bad'):
+    print(good)
+    print(bad, file=sys.stderr)
+    raise RuntimeError('the ugly')
+>>>>>>> 258a535 (pypelib/utils: add logging helper for snakemake rules)
