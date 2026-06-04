123ae57e4561f19d0683ff9f2de52a8b4143b529
Robert <heinro@umich.edu>
2026-06-04 16:01:44 -0400

    pypelib/hmm_asv_test: WIP improvements

--- pypelib/amplicon/hmm_check_asvs.py
+++ pypelib/amplicon/hmm_check_asvs.py
@@ -5,36 +5,62 @@ import argparse
 from statistics import quantiles
 from subprocess import run, DEVNULL
 from tempfile import NamedTemporaryFile
+import sys
 
+from .alignment import HMMRAlignment
 from .hmm import HMM
 
 
 def cli():
     argp = argparse.ArgumentParser(description=__doc__)
     argp.add_argument('hmm_db', help='combined hmm database')
+    argp.add_argument('target_spec', help='e.g. 16S_rRNA_bac.524-776')
     argp.add_argument('asv_fasta', help='Fasta file with ASV sequences')
+    argp.add_argument('--output', help='Output file')
     args = argp.parse_args()
-    main(args.hmm_db, args.fasta)
+    main(args.hmm_db, args.target_spec, args.asv_fasta, output=args.output)
 
 
-def main(hmm_db_path, fasta_path, target_spec, output=None):
+def main(hmm_db_path, target_spec, fasta_path, output=None):
     with open(fasta_path) as ifile:
         fasta_count = sum(1 for line in ifile if line.startswith('>'))
     print(f'Testing {fasta_count} ASVs ...')
 
-    colnames = ['hmmfrom', 'hmm to', 'envfrom', 'env to']
     hmm, _, _ = HMM.spec2targets(target_spec)
-    data = {col: [] for col in colnames}
-    with NamedTemporaryFile('rt') as tbl_out, open(output, 'w') as ofile:
+    with NamedTemporaryFile('rt') as tbl_out:
         cmd = [
             'nhmmscan',
             '--tblout', tbl_out.name,
+            '--cpu', '2',
             hmm_db_path,
             fasta_path,
         ]
         run(cmd, stdout=DEVNULL, check=True)
+        all_alns = HMMRAlignment.load(tbl_out.name)
+        alns = [i for i in all_alns if i.model == hmm]
+        if alns:
+            print(f'Found {len(alns)} matching ASV alignments')
+        else:
+            raise RuntimeError(
+                f'no matching alignments at all out of a total of {len(all_alns)}'
+            )
+
+        cols = ['qname', 'hmmfrom', 'hmmto', 'envfrom', 'envto', 'strand', 'score']
+        ofile = open(output, 'w') if output else sys.stdout
+        ofile.write('\t'.join(cols))
+        ofile.write('\n')
+        for i in alns:
+            row = (str(getattr(i, attr)) for attr in cols)
+            ofile.write('\t'.join(row))
+            ofile.write('\n')
+        if output:
+            ofile.close()
+            print(f'[OK] {output} written')
+
+        """
+
+        print(f'BOEK {len(alns)=}')
         header = tbl_out.readline().rstrip('\n')
-        ofile.write(header + '\n')
         slices = get_cols(colnames, header)
         for lnum, line in enumerate(tbl_out, start=2):
             line = line.rstrip()
@@ -50,25 +76,19 @@ def main(hmm_db_path, fasta_path, target_spec, output=None):
                     raise ValueError(f'on line {lnum}: {e}') from e
                 data[col].append(numval)
                 ofile.write(line + '\n')
+        """
 
-    print(f'Found {len(data[colnames[0]])} matching ASV alignments')
-    print(f'[OK] {output} written')
-
-    if not data[colnames[0]]:
-        raise RuntimeError(
-            f'no rows with alignments to {hmm.name}, total lines: {lnum}'
-        )
-
+    stats_attrs = ('hmmfrom', 'hmmto', 'envfrom', 'envto')
     stats = zip(*[
         [
             min(nums),
             *[int(x) if x.is_integer() else x for x in quantiles(nums)],
             max(nums),
         ]
-        for nums in data.values()
+        for nums in ([getattr(al, attr) for al in alns] for attr in stats_attrs)
     ], strict=True)
 
-    stat_header = ['[stats]', *colnames]
+    stat_header = ['[stats]', *stats_attrs]
     names = ['min', 'Q1', 'Q2', 'Q3', 'max']
     widths = [len(i) for i in stat_header]
     print(*stat_header, sep=' ')
@@ -78,7 +98,7 @@ def main(hmm_db_path, fasta_path, target_spec, output=None):
             for val, width in zip([name, *row], widths)
         ]
         print(*row, sep=' ')
-    print('total:', len(data[colnames[0]]))
+    print('total:', len(alns))
 
 
 def get_cols(colnames, headerline):
